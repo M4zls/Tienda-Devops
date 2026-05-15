fea4698f7f1aca1b767482221469f20cfacec2f1
M4zls <max.palma@duocuc.cl>
2026-05-15 00:19:36 -0400

    cambios aws

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,7 @@ function handleError(res, error, message = "Error interno del servidor") {
   console.error(error);
   res.status(500).json({ message });
 }
+//xd
 
 // Obtener todos los productos
 app.get("/api/productos", async (req, res) => {
